8be076a2c70eae2479773f95dc9ab152da8ffd28
Davey Elder <ian.elder@mail.utoronto.ca>
2023-09-11 12:27:10 -0400

    simplified hydro

--- ieso_capacity_factors.py
+++ ieso_capacity_factors.py
@@ -10,7 +10,6 @@ import json
 import numpy as np
 import xmltodict
 from matplotlib import pyplot
-import solar_capacity_factor
 
 # TODO: replace with something in the translator db
 # times of day and season names for 8760 hours
@@ -21,6 +20,7 @@ def get_ieso_production(download=False):
 
     data = None # do you have to initialize variables in python? feels wrong not to
     if (download):
+
         url = 'http://reports.ieso.ca/public/GenOutputbyFuelHourly/PUB_GenOutputbyFuelHourly_2020.xml'
         xml_data = requests.get(url).content
         data = json.dumps(xmltodict.parse(xml_data))
@@ -31,16 +31,20 @@ def get_ieso_production(download=False):
         file.write(data)
         file.close()
         """
+
         print('Downloaded hourly production data from IESO')
+
     else:
+
         # Pull data from saved json text file
         file = open('ieso_gen_hourly_2020.txt')
         data = json.loads(file.read())
+
         print('Got hourly IESO production data from local cache')
+
     if data == None: return None
 
-    # Data was originally in a horribly nested xml format
-    # data['Document']['DocBody']['DailyData'][day 1-366]['HourlyData'][hour 1 - 24]['FuelTotal'][where 'Fuel' == e.g. 'NUCLEAR']['EnergyValue']['Output']
+
 
     fuels = ['NUCLEAR', 'GAS', 'HYDRO', 'WIND', 'SOLAR', 'BIOFUEL']
     hourly_production = dict()
@@ -50,8 +54,11 @@ def get_ieso_production(download=False):
 
         for day in range(365):
             for hour in range(24):
-                hourly_data = data['Document']['DocBody']['DailyData'][day]['HourlyData'][hour]['FuelTotal']
-                fuel_values = [values for values in hourly_data if values['Fuel'] == fuel]
+
+                # Data was originally in a horribly nested xml format like:
+                # data['Document']['DocBody']['DailyData'][day 1-366]['HourlyData'][hour 1 - 24]['FuelTotal'][any vals where val['Fuel'] == fuel]['EnergyValue']['Output']
+                hour_data = data['Document']['DocBody']['DailyData'][day]['HourlyData'][hour]['FuelTotal']
+                fuel_values = [values for values in hour_data if values['Fuel'] == fuel]
                 fuel_value = float(fuel_values[0]['EnergyValue']['Output'])
                 hourly_production[fuel].append(fuel_value)
 
@@ -71,9 +78,12 @@ wind_total_cap = sum([gen[0] for gen in curs.execute("SELECT exist_cap FROM Exis
 solar_total_cap = sum([gen[0] for gen in curs.execute("SELECT exist_cap FROM ExistingCapacity WHERE regions == 'ON' AND tech LIKE '%SOL%'")])
 hydro_total_cap = sum([gen[0] for gen in curs.execute("SELECT exist_cap FROM ExistingCapacity WHERE regions == 'ON' AND tech LIKE '%HYD%'")])
 
-wind_cf = np.array(hourly_production['WIND']) / wind_total_cap / 1000
+wind_cf = np.array(hourly_production['WIND']) / wind_total_cap / 1000 # MWh/MW to PJ/PJ
 solar_cf = np.array(hourly_production['SOLAR']) / solar_total_cap / 1000
 hydro_cf = np.array(hourly_production['HYDRO']) / hydro_total_cap / 1000
+
+pyplot.plot(hydro_cf)
+pyplot.show()
 
 # Run these separately to keep the database tidy
 for h in range(8760):
@@ -84,6 +94,10 @@ for h in range(8760):
     curs.execute(f"""REPLACE INTO
                  CapacityFactorTech(regions, season_name, time_of_day_name, tech, cf_tech)
                  VALUES('ON', '{seas_8760[h]}', '{tofd_8760[h]}', 'E_SOL_PV', {solar_cf[h]})""")
+for h in range(8760):
+    curs.execute(f"""REPLACE INTO
+                 CapacityFactorTech(regions, season_name, time_of_day_name, tech, cf_tech)
+                 VALUES('ON', '{seas_8760[h]}', '{tofd_8760[h]}', 'E_HYD', {hydro_cf[h]})""")
 
 
     
